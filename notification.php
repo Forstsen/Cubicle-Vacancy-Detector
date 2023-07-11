282af2c2d1e7e3952e0a7c922876a853c66dbe8f
Forstsen <109999732+Forstsen@users.noreply.github.com>
2023-07-11 08:09:03 +0800

    Update notification.php
    remove txt.file

--- notification.php
+++ notification.php
@@ -6,12 +6,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'){
 	
 	$motionStatus = $_POST['motion_status'];
     
-    file_put_contents('motion_status.txt', $motionStatus);
-
-    
-    echo 'Notification received';
-	
-	
+   echo 'Notification received';
 	
 }
 
